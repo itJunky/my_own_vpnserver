3e85b83aaa96bdcf206944966e4076e8e9814781
itj <msemenov@aaa0.ru>
2018-12-29 15:33:47 +0300

    Fix https://github.com/itJunky/my_own_vpnserver/issues/1 (python not exist)

--- run_it.sh
+++ run_it.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+ansible-playbook -i infrastructure.inventory --extra-vars "client_name=testclient" provision.yml --flush-cache
 ansible-playbook -i infrastructure.inventory --extra-vars "client_name=testclient" main.yaml --flush-cache
